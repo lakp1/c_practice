ff86f91aec3e269feacce9a8fed19f7b17e37e4a
lakp1 <1799739687@qq.com>
2026-09-23 18:28:22 +0800

    forth

---
+++ 指针交换.c
@@ -0,0 +1,14 @@
+void swap(int *p1, int *p2) {
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+int main(){
+    int a;
+    int b;
+    printf("请输入两个整数：");
+    scanf("%d %d", &a, &b);
+    swap(&a, &b);
+    printf("%d, %d\n", a, b);
+    return 0;
+}
